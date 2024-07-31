053332ddadcac355fa0c37c1b57ee12250ca42d7
markyasser <82395903+markyasser@users.noreply.github.com>
2024-08-01 02:13:30 +0300

    fixed main

--- main.cpp
+++ main.cpp
@@ -6,11 +6,11 @@ int main()
     NutritionServer &server = NutritionServer::getInstance();
 
     // Example: Add users and create plans
-    User user1("Alice", 25, 65.0, 165.0);
+    User user1("Alice", 25, 65.0, 165.0, "Female", 120, 80, false);
     server.receiveUserInfo(user1);
     server.generateDietPlan(user1);
 
-    User user2("Bob", 30, 75.0, 180.0);
+    User user2("Bob", 30, 75.0, 180.0, "male", 130, 85, true);
     server.receiveUserInfo(user2);
     server.generateDietPlan(user2);
 

032032ef6df366bc77af806292b144e1ed9e6359
markyasser <82395903+markyasser@users.noreply.github.com>
2024-08-03 01:27:55 +0300

    added requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt-get install libasio-dev -y
+sudo apt install build-essential -y
+sudo apt-get install cmake -y
+sudo apt-get install make -y
+sudo apt-get install gcc g++ -y
+sudo apt install screen -y
